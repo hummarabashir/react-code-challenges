602ed84f25eb8769261c1115bb22af19d3747cd1
Hummara Bashir <hummarabashir@gmail.com>
2022-10-24 07:45:06 -0400

    coderenderer

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import ColorRenderer from './color-renderer/ColorRenderer'
+
+function App () {
+  return (
+    <div className='App'>
+      <ColorRenderer />
+    </div>
+  )
+}
+
+export default App
