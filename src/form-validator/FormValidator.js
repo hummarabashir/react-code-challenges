c3a7ba4aa46772a673001642605ac618ab6090bf
Hummara Bashir <hummarabashir@gmail.com>
2022-10-27 07:19:09 -0400

    dog-pics

--- src/form-validator/FormValidator.js
+++ src/form-validator/FormValidator.js
@@ -10,16 +10,20 @@ export default function FormValidator () {
 
   const findErrors = () => {
     const errors = []
-    if (!email || !password || !passwordConfirm) errors.push('All fields must be filled in')
+    if (!email || !password || !passwordConfirm) 
+      errors.push('All fields must be filled in')
     if ([...email].filter(i => i === '@').length !== 1) {
       errors.push('An email must have exactly one @ sign')
     }
-    if (password.length <= 8) errors.push('Passwords must be 8 characters or longer')
-    if (password !== passwordConfirm) errors.push('Passwords must match')
+    if (password.length <= 8) 
+      errors.push('Passwords must be 8 characters or longer')
+    if (password !== passwordConfirm) 
+      errors.push('Passwords must match')
 
     return errors
   }
 
+// Handling Form Submit
   const handleSubmit = e => {
     e.preventDefault()
 
@@ -31,20 +35,11 @@ export default function FormValidator () {
     <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
-      <input
-        type='text' name='email'
-        onChange={e => setEmail(e.target.value)}
-      />
+      <input type='text' name='email' onChange={e => setEmail(e.target.value)} />
       <label htmlFor='password'>Password</label>
-      <input
-        type='password' name='password'
-        onChange={e => setPassword(e.target.value)}
-      />
+      <input type='password' name='password' onChange={e => setPassword(e.target.value)} />
       <label htmlFor='password-confirm'>Confirm Password </label>
-      <input
-        type='password' name='password-confirm'
-        onChange={e => setPasswordConfirm(e.target.value)}
-      />
+      <input type='password' name='password-confirm' onChange={e => setPasswordConfirm(e.target.value)} />
       {message}
       <input type='submit' value='Submit' />
     </form>
