45c966532e94b78707dcd30c5c8274e3684cc5d5
Hummara Bashir <bashirh@state.gov>
2022-10-28 09:14:27 -0400

    new

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+// import ColorRenderer from './color-renderer/ColorRenderer'
+import DarkMode from './dark-mode/DarkMode'
+// import DogPics from './dog-pics/DogPics'
+// import FormValidator from './form-validator/FormValidator'
+// import ScoreKeeper from './score-keeper/ScoreKeeper'
+
+function App () {
+  return (
+    <div className='App'>
+      {/* <ColorRenderer /> */}
+      <DarkMode />
+      {/* <DogPics /> */}
+      {/* <FormValidator /> */}
+      {/* <ScoreKeeper /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/score-keeper/ScoreKeeper.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export default function ScoreKeeper(){
+    const [score, setScore] = useState(parseInt(localStorage.getItem('score')) || 0)
+    useEffect(() => {localStorage.setItem('score', score)}, [score])
+    return(
+        <div>
+            <h1>Score is: {score}</h1>
+            <button onClick={() => setScore(prevScore => prevScore+1)}>+</button>
+            <button onClick={() => setScore(prevScore => prevScore-1)}>-</button>
+        </div>
+    )
+}
