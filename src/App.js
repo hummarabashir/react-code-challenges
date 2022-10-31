4b89607ad0a5f8da1cd4d6c021720c1497dfcd23
Hummara Bashir <hummarabashir@gmail.com>
2022-10-31 11:18:18 -0400

    windowevent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css'
 // import ColorRenderer from './color-renderer/ColorRenderer'
-import DarkMode from './dark-mode/DarkMode'
+// import DarkMode from './dark-mode/DarkMode'
 // import DogPics from './dog-pics/DogPics'
 // import FormValidator from './form-validator/FormValidator'
 // import ScoreKeeper from './score-keeper/ScoreKeeper'
+  import toggleevent from './window-event/toggleevent'
 
 function App () {
   return (
     <div className='App'>
       {/* <ColorRenderer /> */}
-      <DarkMode />
+      {/* <DarkMode /> */}
       {/* <DogPics /> */}
       {/* <FormValidator /> */}
       {/* <ScoreKeeper /> */}
+      <toggleevent />
     </div>
   )
 }
