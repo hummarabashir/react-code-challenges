84c15a98932db0d5173ad447ee242a9e8ba20d3b
Hummara Bashir <bashirh@state.gov>
2022-11-03 11:34:33 -0400

    pixel-art

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css'
 // import ColorRenderer from './color-renderer/ColorRenderer'
-import DarkMode from './dark-mode/DarkMode'
+// import DarkMode from './dark-mode/DarkMode'
 // import DogPics from './dog-pics/DogPics'
 // import FormValidator from './form-validator/FormValidator'
 // import ScoreKeeper from './score-keeper/ScoreKeeper'
+// import Toggleevent from './window-event/Toggleevent'
+// import ColorPicker from './color-picker/ColorPicker'
+import PixelArt from './pixel-art/PixelArt'
 
 function App () {
   return (
     <div className='App'>
       {/* <ColorRenderer /> */}
-      <DarkMode />
+      {/* <DarkMode /> */}
       {/* <DogPics /> */}
       {/* <FormValidator /> */}
       {/* <ScoreKeeper /> */}
+      {/* <Toggleevent /> */}
+      {/* <ColorPicker /> */}
+      <PixelArt />
     </div>
   )
 }
